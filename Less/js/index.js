f7d1c789c285d068b582fb33b5f3ca3ac38bc3f2
wudiljx <13994367734@163.com>
2017-12-29 16:17:18 +0800

    更新内容

---
+++ Less/js/index.js
@@ -0,0 +1,16 @@
+
+var app = new Vue({
+	el : '#app',
+	data : {
+		txt : '头部',
+		show : true
+	},
+	methods: {
+		test : function(){
+
+		},
+		shout : function(){
+			alert("弹出一个框");
+		}
+	}
+})
